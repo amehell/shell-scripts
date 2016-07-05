89dc5638b882fb395267a4a926bb5e952312d695
amehell <amehell@hotmail.com>
2016-07-05 11:20:12 -0500

    Add files via upload

---
+++ filter.sh
@@ -0,0 +1,15 @@
+FILE1=$1
+FILE2=$2
+FILE_MINUS="FILTERED_$1_$2.txt"
+FILE_INTERSECTION="INTERSECTED_$1_$2.txt"
+FILE_GREP1="NAMESONLY_$1"
+grep '^>' $FILE1 > $FILE_GREP1
+while read line; do
+	#echo "Searching: $line in $FILE2"
+	if [ "$(grep -c "$line" $FILE2)" == "0" ] ; then
+		echo "$line" >> $FILE_MINUS
+	else
+		echo "$line" >> $FILE_INTERSECTION
+	fi
+done < $FILE_GREP1
+echo "Done"
